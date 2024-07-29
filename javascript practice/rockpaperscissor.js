7bf948d9e53a178a7cf38087bcff7f1c4bac4eb5
Akili Hall <quaaysan@gmail.com>
2024-07-28 19:13:12 -0500

    Initialize JS project rock paper scissors

---
+++ javascript practice/rockpaperscissor.js
@@ -0,0 +1,11 @@
+/*
+While Game is running
+Obtain user input
+If input is rock, paper or scissors
+continue, else obtain input again
+Get random number, compute 3 different states from number
+Compare input to 1 of 3 states
+Display win, loss, or tie
+*/
+
+console.log("Hello World");
