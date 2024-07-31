16fecd31698c8061929471572f728b1be9603941
Akili Hall <quaaysan@gmail.com>
2024-07-31 12:28:43 -0500

    Change directory structure, add EAS project

---
+++ javascript practice/Etchasketch/EAS.js
@@ -0,0 +1,4 @@
+// etchasketch
+//the goal is to create a series of divs that change color when you hover over them
+//maybe some functionality where the divs are animated and fade to white
+//achieving the shake feature of the real toy
