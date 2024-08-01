6a1a5ae55cbda0519e6413942d33cf8a9409a1fd
Akili Hall <quaaysan@gmail.com>
2024-08-01 11:59:06 -0500

    Add operator functions: add, subtract, multiple, divide

--- javascript practice/Calculator/calculator.js
+++ javascript practice/Calculator/calculator.js
@@ -5,3 +5,19 @@ Screen
 1 2 3 x
 < 0 = /
 */
+
+function add(a,b) {
+    return a+b ;
+}
+
+function subtract(a,b) {
+    return a-b ;
+}
+
+function multiply(a,b) {
+    return a*b ;
+}
+
+function divide(a,b) {
+    return a/b ;
+}
