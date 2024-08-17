c36eb6d56b0b7cf474c6e3cd6d0485fbf4a089c4
Akili Hall <quaaysan@gmail.com>
2024-08-17 12:42:17 -0500

    create stockpicker

---
+++ Ruby/ruby_projects/stockpicker.rb
@@ -0,0 +1,6 @@
+#given a list of numbers, go through each item
+#for each item, go through the rest of the list
+#find the subsequent number that leads to the largest difference
+#check the index for the item and the subsequent number
+#if there is a bigger difference, keep the biggest
+#difference = subsequent number - item
