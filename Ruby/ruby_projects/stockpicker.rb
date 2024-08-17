aca27b147ff198ba4f16c4875a61d6e963ccd43d
Akili Hall <quaaysan@gmail.com>
2024-08-17 14:08:54 -0500

    complete stockpicker

--- Ruby/ruby_projects/stockpicker.rb
+++ Ruby/ruby_projects/stockpicker.rb
@@ -4,3 +4,19 @@
 #check the index for the item and the subsequent number
 #if there is a bigger difference, keep the biggest
 #difference = subsequent number - item
+def stockpicker(prices)
+  daypair = [0, 0]
+
+  for buy in 0..prices.length-1
+    for sell in 0..prices.length-1
+      if sell > buy
+        if prices[sell] - prices[buy] > prices[daypair[1]] - prices[daypair[0]]
+          daypair = [buy, sell]
+        end
+      end
+    end
+  end
+  puts "buy on day #{daypair[0]}, sell on day #{daypair[1]}"
+  daypair
+end
+stockpicker([17, 3, 6, 9, 15, 8, 6, 1, 10])
