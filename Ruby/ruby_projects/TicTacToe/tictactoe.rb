6efd9c477b5a450030d521906b9ebd6dd6932456
Akili Hall <quaaysan@gmail.com>
2024-09-02 21:20:03 -0500

    pseudocode for board object

--- Ruby/ruby_projects/TicTacToe/tictactoe.rb
+++ Ruby/ruby_projects/TicTacToe/tictactoe.rb
@@ -3,18 +3,36 @@
 
 # ## Board object
 # slots for each mark
-# winner variable
+# winner variable (should I make this nil instead of false?)
 # hash that converts position to slot number
 # combinations for checking winner
 
 # method to get the winner
+# #return winner
 # change the winner
+# #winner = !winner
 # check the winner
+# #for every combo in combolist
+# #get slots for combo
+# #if the slots are all the same, there's a winner
+# #the winner is the var in all the slots
+# #return nil for no winner, true for p1, false for p2
 # display the board current
+# #puts the first line, then the second, third, zhush it up
 # convert method guide to mark
+# #this is assuming that the player is only allowed to type specific things
+# #return the value of the key, the key is the player input
 # make mark (set)
-# get make
+# #check if slot is marked (get mark)
+# #if slot isn't marked, add the players mark (turn)
+# #if is, output error message
+# get mark
+# #I guess I should put this one first
+# #return the slot at the specific index
+# #maybe I'll need the conversion method
 # I have a tie method, but maybe I wont need it.
+
+
 
 # Game object
 # turn var to keep track
