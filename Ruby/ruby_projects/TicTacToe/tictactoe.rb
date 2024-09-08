1d1ed99df9868e54fa8051be459c0c08a5e3e764
Akili Hall <quaaysan@gmail.com>
2024-09-07 19:02:01 -0500

    update position and combo for Board

--- Ruby/ruby_projects/TicTacToe/tictactoe.rb
+++ Ruby/ruby_projects/TicTacToe/tictactoe.rb
@@ -2,8 +2,17 @@ class Board
   def initialize
     @slots = Array.new(9)
     @winner = nil
-    @position = {"temp":true}
-    @combos = []
+    @position = {
+      "topleft": 0, "top": 1,
+      "topright": 2, "left": 3,
+      "center": 4, "right": 5,
+      "botleft": 6, "bottom": 7, "botright": 8}
+    @combos = [
+      [0, 1, 2], [3, 4, 5],
+      [6, 7, 8], [0, 3, 6],
+      [1, 4, 7], [2, 5, 8],
+      [2, 4, 6], [0, 4, 8]
+    ]
   end
 end
 
